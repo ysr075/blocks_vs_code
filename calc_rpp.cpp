a0fba5b5b9b0cc6b4692df85b8582a938e165741
ysr075 <74384666+ysr075@users.noreply.github.com>
2021-03-10 00:34:25 +0100

    Update calc_rpp.cpp

--- calc_rpp.cpp
+++ calc_rpp.cpp
@@ -4,45 +4,38 @@ float num1;
 float num2;
 int choice;
 
-void default_Attribute()
-{
+void default_Attribute() {
     std::cout << "\n" << "num1: "; std::cin >> num1; 
     std::cout << "num2: "; std::cin >> num2; 
 }
 
-void plus()
-{
+void plus() {
     default_Attribute();
     float total = num1 + num2;
     std::cout << "total: " << total << std::endl;
 }
 
-void min()
-{
+void min() {
     default_Attribute();
     float total = num1 - num2;
     std::cout << "total: " << total << std::endl;
 }
 
-void maal()
-{
+void maal() {
     default_Attribute();
     float total = num1 * num2;
     std::cout << "total: " << total << std::endl;
 }
 
-void delen()
-{
+void delen() {
     default_Attribute();
     float total = num1 / num2;
     std::cout << "total: " << total << std::endl;
 }
 
-int main() 
-{
+int main() {
     std::cout << "\n" << "1.plus/2.min/3.maal/4.delen: "; std::cin >> choice;
-    switch (choice)
-    {
+    switch (choice) {
 	    case 1:
 	        plus();
 	        break;
